80d570e5711110fddf59a5083c4ef3487be14d17
Tresor Langombo <12345678+username@users.noreply.github.com>
2026-06-03 20:50:51 -0700

    Add Terraform configuration for week7

---
+++ terraform/week7/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "my_ip_cidr" {
+  description = "Your public IP CIDR (e.g., 203.0.113.5/32)"
+  type        = string
+}
